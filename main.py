99a026ce332f1ad04193b6ea54e300957544defa
razsamuel <razek258@gmail.com>
2024-01-11 12:06:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def read_file_content(file_name):
     # Potentially vulnerable code
-    file_path = file_name
+    file_path = os.path.join('/path/to/files', file_name)  # Fixed directory
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     return content
