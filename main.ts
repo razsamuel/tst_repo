66bfa4cfbd1e911e821e5dcc50cfd269681a9462
razsamuel <razek258@gmail.com>
2024-01-11 11:54:03 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
 function readFileContent(fileName) {
-  const filePath = '/path/to/files/' + fileName; // Potentially vulnerable code
+  const filePath = fileName; // Potentially vulnerable code
   const content = fs.readFileSync(filePath, 'utf-8');
   return content;
 }
 
 // Simulating user input (replace this with your actual user input)
-const userInput = '../somefile.txt';
+const userInput = prompt("Please enter file name");;
 
 // Potentially vulnerable code: using user input in a filesystem operation
 const fileContent = readFileContent(userInput);
